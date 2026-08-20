f14f3650558e343e46c17b691b0f74e6186e2140
splicybuild <alansam.net@gmail.com>
2026-08-20 04:02:50 +0100

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach((link)=>{link.addEventListener('click',(e)=>{const id=link.getAttribute('href');if(id==='#')return;const target=document.querySelector(id);if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth',block:'start'});}})});
